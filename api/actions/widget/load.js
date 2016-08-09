52fbf1cb70cc6015dfb069439399771aac0b55a8
Chris Hasson <hassoncs@gmail.com>
2016-08-08 20:15:30 -0700

    Fix bug breaking front-end

--- api/actions/widget/load.js
+++ api/actions/widget/load.js
@@ -1,7 +1,11 @@
 
+const pogobuf = require('pogobuf');
+const POGOProtos = require('node-pogo-protos');
+const botState = require('../../bot/botState');
+
 export default function load(req) {
   return new Promise((resolve, reject) => {
-    resolve({});
-    return;
+    resolve(botState);
   });
 }
+
