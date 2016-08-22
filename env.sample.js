628c1abf7c17f5de10843fb09f76c3213cba08e7
Chris Hasson <hassoncs@gmail.com>
2016-08-22 16:54:54 -0700

    Increase delays and decrease walk speed

--- env.sample.js
+++ env.sample.js
@@ -4,8 +4,8 @@ module.exports = {
   password: '',
   maxTransferCP: 1200,
   delayBetweenRecyclingItems: 3000,
-  delayBetweenEvolves: 20000,
-  delayBetweenTransfers: 3000,
+  delayBetweenEvolves: 25000,
+  delayBetweenTransfers: 3500,
   maxPokemonPercentFullBeforeTransfer: 0.75,
-  walkSpeedMetersPerSecond: 4.7, // human speed is 1.4 - 2.5?
+  walkSpeedMetersPerSecond: 2.4, // human speed is 1.4 - 2.5?
 };
