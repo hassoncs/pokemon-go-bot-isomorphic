ee64ccbd08dc99e231669a9b6c4b64dcbdd5d793
Chris Hasson <hassoncs@gmail.com>
2016-08-25 15:05:07 -0700

    Improve the StatsSummary logging
    Also make it run more often

--- api/bot/workers/StatsSummaryWorker.js
+++ api/bot/workers/StatsSummaryWorker.js
@@ -5,7 +5,7 @@ export default class StatsSummaryWorker extends TickWorker {
   getConfig() {
     return {
       needsLogIn: false,
-      actEvery: 20000,
+      actEvery: 10 * 1000,
     };
   }
 
@@ -13,14 +13,14 @@ export default class StatsSummaryWorker extends TickWorker {
     const pokemonPerHour = this.stats.pokemonPerHour.average;
     const avgXPPerHour = this.stats.xpPerHour.average;
 
-    console.log(`Averaging ${(avgXPPerHour).toFixed().green} xp per hour\
- and ${(pokemonPerHour).toFixed().green} pokemon per hour`);
-
     if (!this.state.inventory.player) return;
     const {currentLevelXP, xpNeededForNextLevel} = this.state.inventory.player;
 
     const xpNeeded = xpNeededForNextLevel - currentLevelXP;
     const hoursTilNextLevel = xpNeeded / avgXPPerHour;
-    console.log(`Level up in ${(hoursTilNextLevel * 60).toFixed(0)} minutes`);
+
+    console.log(`Averaging ${(avgXPPerHour).toFixed().green} xp/hr\
+ and ${(pokemonPerHour).toFixed().green} pokemon/hr.\
+ Level up in ~${(hoursTilNextLevel * 60).toFixed(0)} minutes`);
   }
 }
